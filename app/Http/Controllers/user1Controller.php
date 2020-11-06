368a537e00b8912ee229435c6126b1fb4737d297
COROTANjayson <jayzoon19@gmail.com>
2020-11-06 14:21:34 +0800

    Updated

--- app/Http/Controllers/user1Controller.php
+++ app/Http/Controllers/user1Controller.php
@@ -5,36 +5,52 @@
 	use Illuminate\Http\Response;
 	//use App\Models\User;  
 	use App\Traits\ApiResponser; 
+	use App\Services\User1Service;
 	use DB; 
 
-	Class UserController extends Controller {
-		use ApiResponser; 
-
+	Class User1Controller extends Controller {
+		use ApiResponser;  
 		private $request;
-	
-		public function __construct(Request $request){
-			
-			$this->request = $request;
+		
+
+		/**
+		* The service to consume the User1 Microservice
+		* @var User1Service
+		*/
+		
+		public $user1Service;
+		
+		/**
+		* Create a new controller instance
+		* @return void
+		*/
+		
+		public function __construct(User1Service $user1Service){
+			$this->user1Service = $user1Service;
 		}
 
 		public function show(){
-			 
+			 return $this->successResponse($this->user1Service->obtainUsers1());
 		}
 
 		public function add(Request $request){
-			
+			return $this->successResponse($this->user1Service->createUser1($request->all(),Response::HTTP_CREATED));
 		}
 
 		public function index($id){
+			return $this->successResponse($this->user1Service->obtainUser1($id));
 			
 		}
 
 		public function update(Request $request, $id){
-
+			return $this->successResponse($this->user1Service->editUser1($request->all(),$id));
+	
 		
 		}
 
 		public function delete($id){
+			return $this->successResponse($this->user1Service->deleteUser1($id));
+}
 		}
 		
 
